474da51c0abaad80670061f714958d60f043d0d0
Denis Kent <denis.kent@snyk.io>
2024-02-25 09:42:38 +0000

    fix: setup port correctly

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # go-hello-world
+
+The server build command:
+```
+go build -tags netgo -ldflags '-s -w' -o app
+```
+
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
@@ -12,6 +13,10 @@ func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/a", HelloWorldHandler)
 
-	fmt.Println("Server is running on http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	fmt.Println("Server is running on http://localhost:" + port)
+	http.ListenAndServe(":"+port, nil)
 }
